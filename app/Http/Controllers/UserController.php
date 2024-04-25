8f77434b89f42be66b01174f64037d3ef5f262f7
Michael Jay Lupos <luposmichaeljay@gmail.com>
2024-04-25 12:28:15 +0800

    [auth]: added login/register and sanctum

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+
+        $user = User::find(['email' => $request->email]);
+
+        if (Hash::check($request->password, $user->password)) {
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login successful.',
+                'user' => $user,
+                'token' => $token
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Invalid credentials. Please try again.'
+        ]);
+    }
+
+
+    public function register(RegisterRequest $request)
+    {
+        $password = Hash::make($request->password);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $password
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User created successfully.',
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+}
